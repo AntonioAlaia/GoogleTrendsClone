3a1f9e1f1f4adb02e78ee476ef3aaaf4c2984e2e
Antonio Alaia <alaia.comunicazione@gmail.com>
2022-02-28 18:53:51 +0100

    First attempts_creating dataset

---
+++ DXTX.R
@@ -0,0 +1,29 @@
+library(gtrendsR)
+library(tidyverse)
+library(rvest)
+
+
+lista_nera <- "https://www.patriaindipendente.it/persone-e-luoghi/inchieste/la-galassia-nera-su-facebook/"
+
+download.file(url = lista_nera, 
+              destfile = here::here("ListaNera.html"))
+
+scrap_listaNera <- read_html(here::here("ListaNera.html")) %>% 
+  html_elements(css = "p:nth-child(24) em , p b") %>% 
+  html_text(trim = TRUE)
+
+view(scrap_listaNera)
+
+divideR <- function(x,n) split(x, cut(seq_along(x), n, labels = FALSE)) 
+
+dvd_listaNera <- divideR(scrap_listaNera, 7)
+
+as.list(dvd_listaNera[1]) 
+
+
+
+test1_listaNera <- gtrends(keyword = c("Forza Nuova","CasaPound Italia","Comunità militanti","Lealtà – Azione"), geo = "IT", time = "2020-01-01 2022-02-28")
+
+view(test1_listaNera$related_queries)
+
+(test1_listaNera$interest_over_time)
