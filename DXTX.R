fe9835c96af2a7881415b2fc73dd5843c59ce31c
Antonio Alaia <alaia.comunicazione@gmail.com>
2022-03-03 12:27:56 +0100

    Solved merging issues :dart:

--- DXTX.R
+++ DXTX.R
@@ -83,5 +83,67 @@ group_2_onlytime <- gtrends(keyword = DATASET$'2',
                             
 )
 
-##NOT SOLVED!!!
+group_3_onlytime <- gtrends(keyword = DATASET$'3',
+                            geo = "IT",
+                            time = "2020-01-01 2022-02-28",
+                            onlyInterest = TRUE
+                            
+)
+
+group_4_onlytime <- gtrends(keyword = DATASET$'4',
+                            geo = "IT",
+                            time = "2020-01-01 2022-02-28",
+                            onlyInterest = TRUE
+                            
+)
+
+group_5_onlytime <- gtrends(keyword = DATASET$'5',
+                            geo = "IT",
+                            time = "2020-01-01 2022-02-28",
+                            onlyInterest = TRUE
+                            
+)
+
+group_6_onlytime <- gtrends(keyword = DATASET$'6',
+                            geo = "IT",
+                            time = "2020-01-01 2022-02-28",
+                            onlyInterest = TRUE
+                            
+)
+
+group_7_onlytime <- gtrends(keyword = DATASET$'7',
+                            geo = "IT",
+                            time = "2020-01-01 2022-02-28",
+                            onlyInterest = TRUE
+                            
+)
+
+
+
+group_data1 <- as.data.frame(group_1_onlytime$interest_over_time)
+
+group_data2 <- as.data.frame(group_2_onlytime$interest_over_time)
+
+group_data3 <- as.data.frame(group_3_onlytime$interest_over_time)
+
+group_data4 <- as.data.frame(group_4_onlytime$interest_over_time)
+
+group_data5 <- as.data.frame(group_5_onlytime$interest_over_time)
+
+group_data6 <- as.data.frame(group_6_onlytime$interest_over_time)
+
+group_data7 <- as.data.frame(group_7_onlytime$interest_over_time)
+
+
+DATASET_TOTAL <- rbind(group_data1, group_data2, group_data3, group_data4, group_data5, group_data6, group_data7)
+
+DATASET_TOTAL
+
+view(trial)
+
+view(x)
+
+ggplot(x, aes(x=date, y=hits)) +
+  geom_point() +
+  theme_bw()
 
