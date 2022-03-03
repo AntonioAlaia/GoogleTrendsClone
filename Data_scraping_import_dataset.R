f325e20785c7e5d5401a36809a1fdb6b210a730c
Antonio Alaia <alaia.comunicazione@gmail.com>
2022-03-04 00:50:39 +0100

    gtrendsR for RELATED QUERIES

--- Data_scraping_import_dataset.R
+++ Data_scraping_import_dataset.R
@@ -25,3 +25,41 @@ divideR <- function(x,n) split(x, cut(seq_along(x), n, labels = FALSE))
 dvd_listaNera <- divideR(scrap_listaNera, 7)
 
 view(dvd_listaNera)
+
+
+#Using gtrendsR to get the queries 
+
+
+test1_listaNera <- gtrends(keyword = dvd_listaNera$'1', 
+                           geo = "IT",
+                           time = "2020-01-01 2022-02-28"
+)
+test2_listaNera <- gtrends(keyword = dvd_listaNera$'2',
+                           geo = "IT",
+                           time = "2020-01-01 2022-02-28"
+)
+test3_listaNera <- gtrends(keyword = dvd_listaNera$'3',
+                           geo = "IT",
+                           time = "2020-01-01 2022-02-28"
+)
+test4_listaNera <- gtrends(keyword = dvd_listaNera$'4',
+                           geo = "IT",
+                           time = "2020-01-01 2022-02-28"
+)
+test5_listaNera <- gtrends(keyword = dvd_listaNera$'5',
+                           geo = "IT",
+                           time = "2020-01-01 2022-02-28"
+)
+test6_listaNera <- gtrends(keyword = dvd_listaNera$'6',
+                           geo = "IT",
+                           time = "2020-01-01 2022-02-28"
+)
+test7_listaNera <- gtrends(keyword = dvd_listaNera$'7',
+                           geo = "IT",
+                           time = "2020-01-01 2022-02-28"
+)
+test8_listaNera <- gtrends(keyword = c("8chan","8kun","pillola rossa","olocausto italiano","grande sostituzione"),
+                           geo = "IT",
+                           time = "2020-01-01 2022-02-28"
+)
+                            
