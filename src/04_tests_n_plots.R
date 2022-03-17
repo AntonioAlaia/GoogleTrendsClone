9ddaeda0851bfbd98fdf85c597bc801defd01d9a
Antonio Alaia <99255000+AntonioAlaia@users.noreply.github.com>
2022-03-17 14:41:36 +0100

    fixed plots and exported :ok_hands:

--- src/04_tests_n_plots.R
+++ src/04_tests_n_plots.R
@@ -1,23 +1,12 @@
-library(gtrendsR)
 library(tidyverse)
-library(rvest)
-library(stringr)
 library(rio)
 
 
 COMPLETE_DATASET_regex <- import("Complete_dataset(cleanedREGEXS).csv")
 
 
-ggplot(COMPLETE_DATASET_regex, aes(x=date, y=hits)) +
-  geom_point(aes(date, hits,color = keyword), position = 'jitter') +
-  geom_smooth(method=lm , color="red", fill="#69b3a2", se=TRUE) +
-  theme(legend.position = "none")
-theme_bw()
 
-
-##
-
-ggplot(COMPLETE_DATASET_regex, aes(x=date, y=hits) ) +
+density_plot <- ggplot(COMPLETE_DATASET_regex, aes(x=date, y=hits) ) +
   stat_density_2d(aes(fill = ..density..), geom = "raster", contour = FALSE) +
   scale_fill_distiller(palette=4, direction=1) +
   scale_x_datetime(date_breaks = "3 months") +
@@ -26,4 +15,18 @@ ggplot(COMPLETE_DATASET_regex, aes(x=date, y=hits) ) +
     legend.position='none'
   )
 
+ggsave("Density Plot.png", plot = density_plot, width = 30, height = 20, units = "cm")
 
+
+####
+
+
+
+linear_plot <- ggplot(COMPLETE_DATASET_regex, aes(x=date, y=hits)) +
+  geom_point(aes(date, hits,color = keyword), position = 'jitter') +
+  geom_smooth(method=lm , color="red", fill="#69b3a2", se=TRUE) +
+  theme(legend.position = "none")
+theme_bw()
+
+
+ggsave("Linear Plot.png", plot = linear_plot, width = 30, height = 20, units = "cm")
