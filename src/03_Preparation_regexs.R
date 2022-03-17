eeb40422b1c13a3d40b508410c955a1b151145af
Antonio Alaia <99255000+AntonioAlaia@users.noreply.github.com>
2022-03-17 10:45:42 +0100

    adding new list + regexs

--- src/03_Preparation_regexs.R
+++ src/03_Preparation_regexs.R
@@ -6,21 +6,27 @@ library(rio)
 
 COMPLETE_DATASET <- import("Complete_dataset.csv")
 
-view(COMPLETE_DATASET)
 
-COMPLETE_DATASET$keyword <- str_remove_all(COMPLETE_DATASET$keyword,"mussolini")
-COMPLETE_DATASET$keyword <- str_remove_all(COMPLETE_DATASET$keyword,"Mussolini")
-COMPLETE_DATASET$keyword <- str_remove_all(COMPLETE_DATASET$keyword,"Muti")
-COMPLETE_DATASET$keyword <- str_remove_all(COMPLETE_DATASET$keyword,"forza italia")
-COMPLETE_DATASET$keyword <- str_remove_all(COMPLETE_DATASET$keyword,"meloni")
-COMPLETE_DATASET$keyword <- str_remove_all(COMPLETE_DATASET$keyword,"^Benito")
-COMPLETE_DATASET$keyword <- str_remove_all(COMPLETE_DATASET$keyword,"mussolini")
-COMPLETE_DATASET$keyword <- str_remove_all(COMPLETE_DATASET$keyword,"Camice Nere")
-
-
-
+COMPLETE_DATASET$keyword <- str_remove_all(COMPLETE_DATASET$keyword,"mussolini|Mussolini|Muti|forza italia|meloni|^Benito|Camice Nere|- Aforismi|- DUX|	
+BENITO MUSSOLINI|Camicie Nere|carlo taormina| ")
 
 
 COMPLETE_DATASET <- COMPLETE_DATASET[-which(COMPLETE_DATASET$keyword==""), ]
 
+view(COMPLETE_DATASET)
 
+export(COMPLETE_DATASET, "Complete_dataset(cleanedREGEXS).csv")
+
+
+
+##Aggiunta nuova lista
+
+
+stormfront <- "https://www.stormfront.org/forum/t726675/"
+
+download.file(url = lista_nera, 
+              destfile = here::here("ListaNera.html"))
+
+scrap_listaNera <- read_html(here::here("ListaNera.html")) %>% 
+  html_elements(css = "p:nth-child(24) em , p b") %>% 
+  html_text(trim = TRUE)
